ca18d4bd1037f94aa74bb8cc10d058e0c2a527d2
ckckckcz <rriovld@gmail.com>
2025-01-05 14:06:34 +0700

    replace MegaMenu component with logo update in Navbar and adjust styles

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,12 @@
 import avatar from "../assets/img/avatar.png";
+import Logo from "../assets/img/logo.png";
 export default function Navbar() {
     return(
         <>
             <nav className="bg-white border-gray-200">
                 <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-6">
                     <a href="https://flowbite.com" className="flex items-center space-x-3 rtl:space-x-reverse">
-                        <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="View Point Logo" />
+                        <img src={Logo} className="h-12" alt="View Point Logo" />
                         <span className="self-center text-2xl font-semibold whitespace-nowrap lg:block hidden">View Point</span>
                     </a>
                     <div className="flex items-center space-x-5 rtl:space-x-reverse">
@@ -18,7 +19,7 @@ export default function Navbar() {
                                 <svg className="w-6 h-6 text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
                                     <path fill-rule="evenodd" d="M4 4a1 1 0 0 1 1-1h1.5a1 1 0 0 1 .979.796L7.939 6H19a1 1 0 0 1 .979 1.204l-1.25 6a1 1 0 0 1-.979.796H9.605l.208 1H17a3 3 0 1 1-2.83 2h-2.34a3 3 0 1 1-4.009-1.76L5.686 5H5a1 1 0 0 1-1-1Z" clip-rule="evenodd"/>
                                 </svg>
-                                <span className="top-1 left-6 absolute  w-3.5 h-3.5 bg-blue-700 border-2 border-whit rounded-full"></span>
+                                <span className="top-1 left-6 absolute  w-3.5 h-3.5 bg-[#00165b] border-2 border-whit rounded-full"></span>
                             </div>
                         </div>
                         <div className="border border-gray-200 h-7"></div>
