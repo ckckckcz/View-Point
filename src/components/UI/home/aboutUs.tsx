1546cc863230ee08cb9c5e4a95b1918104eba1d8
ckckckcz <rriovld@gmail.com>
2025-01-05 14:31:53 +0700

    update AboutUs and Home components for improved layout and content

--- src/components/UI/home/aboutUs.tsx
+++ src/components/UI/home/aboutUs.tsx
@@ -1,7 +1,7 @@
 export default function AboutUs() {
     return(
         <section className="bg-white">
-            <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
+            <div className="py-8 px-4 mx-auto max-w-screen-xl lg:text-center text-left lg:py-16">
                 <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl">We invest in the world’s potential</h1>
                 <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
                 <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
