bbcf2ced85030ae390b044ea9eb0cae594589d93
ckckckcz <rriovld@gmail.com>
2025-01-05 14:32:42 +0700

    update Home component to increase section height for better visual impact

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,7 +2,7 @@ import About from "../components/UI/home/aboutUs";
 const Home = () => {
     return(
         <>
-            <section className="bg-center bg-no-repeat bg-cover bg-[url('https://cdn.pixabay.com/photo/2016/09/12/20/44/horse-race-1665688_960_720.jpg')] bg-gray-700 bg-blend-multiply lg:h-screen lg:w-screen h-[600px] flex items-center justify-center">
+            <section className="bg-center bg-no-repeat bg-cover bg-[url('https://cdn.pixabay.com/photo/2016/09/12/20/44/horse-race-1665688_960_720.jpg')] bg-gray-700 bg-blend-multiply lg:h-screen lg:w-screen h-[650px] flex items-center justify-center">
                 <div className="lg:text-center text-left px-4 mx-auto max-w-screen-xl">
                     <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">The Track Is Calling – Saddle Up!</h1>
                     <p className="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">
